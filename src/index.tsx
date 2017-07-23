552139a763ffae37afd8722088409c9515b2f7c7
Frederick <frederick@gnodtke.net>
2017-07-23 18:38:48 +0200

    WIP: Redirects

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import { Router } from "react-router";
 import { observer } from "mobx-react";
 import DevTools from "mobx-react-devtools";
 import { App } from "app";
@@ -9,6 +9,7 @@ import "semantic-ui-css/semantic.css";
 import "style.scss";
 import { i18n } from "i18n";
 import { I18nextProvider } from "react-i18next";
+import { browserHistory } from "browser-history";
 
 import { store } from "./store";
 
@@ -16,9 +17,9 @@ async function main() {
     ReactDOM.render(
         <div>
             <I18nextProvider i18n={i18n}>
-                <BrowserRouter>
+                <Router history={browserHistory}>
                     <App />
-                </BrowserRouter>
+                </Router>
             </I18nextProvider>
             {!isProductionEnvironment() && <DevTools />}
         </div>,
--- src/store/signup.ts
+++ src/store/signup.ts
@@ -1,6 +1,7 @@
 import { observable, computed, action } from "mobx";
 import { api } from "./api";
 import { bind } from "bind-decorator";
+import { store } from ".";
 
 export class SignupStore {
     @observable public failed = false;
@@ -13,6 +14,7 @@ export class SignupStore {
         const { data } = result;
         if (result.okay) {
             this.succeeded = true;
+            await store.login.onLogin(email, password);
         } else {
             this.failed = true;
         }
